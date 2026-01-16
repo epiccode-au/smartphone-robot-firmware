f12bb25b87a2858b27599ec0c298750d620b6f1c
Michael <git@epic-code.com.au>
2026-01-16 20:08:59 +1100

    Minor fixes to comm link test script

--- extras/read-log.sh
+++ extras/read-log.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 dev=${BOT_SERIAL_DEV:-/dev/ttyACM0}
-stty -F "$dev" 115200 cs8 -cstopb -parenb
+stty -F "$dev" 115200 cs8 raw -cstopb -parenb -echo
+timeout 0.5 cat "$dev" &
 echo -ne '\xfe\x00xx\xff' > "$dev"
-timeout 0.5 cat "$dev"
+wait
